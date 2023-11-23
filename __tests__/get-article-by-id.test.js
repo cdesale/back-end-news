ee2d58e44d27fc0a18306df65eae4589dc1c2f69
Chaitali Desale <chaitali.desale92@gmail.com>
2023-11-23 12:46:48 +0000

    Addresses the 1st code review comments - Adds a seperate test - Updates endpoint.JSON

--- __tests__/get-article-by-id.test.js
+++ __tests__/get-article-by-id.test.js
@@ -21,7 +21,6 @@ describe("GET /api/articles/:article_id", () => {
       .then(({ body }) => {
         const { article } = body;
         expect(article.article_id).toBe(1);
-        expect(article.comment_count).toBe(11);
         expect(article).toMatchObject({
           title: expect.any(String),
           topic: expect.any(String),
@@ -33,6 +32,25 @@ describe("GET /api/articles/:article_id", () => {
         });
       });
   });
+    test("200: responds with the article object containing additional property called comment_count for the given article id", () => {
+      return request(app)
+        .get("/api/articles/1")
+        .expect(200)
+        .then(({ body }) => {
+          const { article } = body;
+          expect(article.article_id).toBe(1);
+          expect(article.comment_count).toBe(11);
+          expect(article).toMatchObject({
+            title: expect.any(String),
+            topic: expect.any(String),
+            author: expect.any(String),
+            body: expect.any(String),
+            created_at: expect.any(String),
+            votes: expect.any(Number),
+            article_img_url: expect.any(String),
+          });
+        });
+    });
   test("400: responds with an error msg when using invalid article id", () => {
     return request(app)
       .get("/api/articles/i")
