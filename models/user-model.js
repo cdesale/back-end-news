081580d122a6309ff0e84ca2493b1151ed2cb670
Chaitali Desale <chaitali.desale92@gmail.com>
2023-11-24 17:03:04 +0000

    Adds GET /api/users/:username API

--- models/user-model.js
+++ models/user-model.js
@@ -3,3 +3,17 @@ const db = require("../db/connection");
 exports.selectUsers = () => {
   return db.query(`SELECT * FROM users`).then((data) => data.rows);
 };
+
+exports.selectUserByUsername = (username) => {
+  return db
+    .query(`SELECT username, avatar_url, name FROM users WHERE username = $1`, [
+      username,
+    ])
+    .then(({ rows }) => {
+      if (rows.length === 0) {
+        return Promise.reject({ status: 404, msg: "not found" });
+      }
+
+      return rows[0];
+    });
+};
